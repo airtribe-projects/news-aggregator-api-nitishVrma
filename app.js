a2ce31e7ae8722e7b78c8430052b77ace4335612
Nitish Verma <nitishverma2371997@gmail.com>
2025-07-19 23:54:11 +0530

    registration_login

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth'); // Import the auth routes
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI, { /* options */ })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.error(err));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/api/auth', authRoutes);
+app.get("/run", (req, res) => {
+  console.log("running /run")
+  res.json("Hello Runner ")
+})
+
 app.listen(port, (err) => {
-    if (err) {
-        return console.log('Something bad happened', err);
-    }
-    console.log(`Server is listening on ${port}`);
+  if (err) {
+    return console.log('Something bad happened', err);
+  }
+  console.log(`Server is listening on ${port}`);
 });
-
-
 
 module.exports = app;
